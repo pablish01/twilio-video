28a21d10e201936230d771dd4072f03ec6705223
Pablo Landaeta <plandaeta@imed.cl>
2022-08-08 23:06:24 -0400

    new docs

--- scripts/docs.js
+++ scripts/docs.js
@@ -158,14 +158,14 @@ function transform(file) {
   var body = $('body');
   var bodyHtml = body.html();
   body.html(bodyHtml + [
-    "<script>",
-      "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){",
-      "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),",
-      "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)",
-      "})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');",
-      "ga('create', 'UA-2900316-33', 'auto');",
-      "ga('send', 'pageview');",
-    "</script>"
+    '<script>',
+    '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){',
+    '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),',
+    'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)',
+    '})(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');',
+    'ga(\'create\', \'UA-2900316-33\', \'auto\');',
+    'ga(\'send\', \'pageview\');',
+    '</script>'
   ].join(''));
 
   file.contents = new Buffer($.html());
